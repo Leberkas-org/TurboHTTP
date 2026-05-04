5f884529c0df9fa64889ea2ae38c42b0fd27a631
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-05-04 09:59:34 +0200

    fix(ci): integrate deps commit type

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,10 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-  ignores: [(commit) => /^Signed-off-by: dependabot\[bot\]/m.test(commit)]
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', 'fix', 'perf', 'docs', 'chore',
+      'refactor', 'test', 'ci', 'build', 'deps',
+    ]],
+  },
+  ignores: [(commit) => /^Signed-off-by: dependabot\[bot\]/m.test(commit)],
 };
