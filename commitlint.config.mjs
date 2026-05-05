9f4ed18f87f31b8235fea986b4984c3f058e0ee1
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-05-05 18:16:12 +0200

    fix(lint-config): Disable case rules

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -5,6 +5,11 @@ export default {
       'feat', 'fix', 'perf', 'docs', 'chore',
       'refactor', 'test', 'ci', 'build', 'deps',
     ]],
+    'header-case': [0],
+    'body-case': [0],
+    'subject-case': [0],
+    'scope-case': [0],
+    'type-case': [0],
   },
   ignores: [(commit) => /^Signed-off-by: dependabot\[bot\]/m.test(commit)],
 };
