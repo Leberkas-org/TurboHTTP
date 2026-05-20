42072228166b95f38017a154e6071a277065d75a
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-05-20 15:38:17 +0200

    feat(features): add ITlsHandshakeFeature interface and implementation

---
+++ src/TurboHTTP/Context/Features/TlsHandshakeFeature.cs
@@ -0,0 +1,12 @@
+using System.Net.Security;
+using System.Security.Authentication;
+
+namespace TurboHTTP.Context.Features;
+
+internal sealed class TlsHandshakeFeature : ITlsHandshakeFeature
+{
+    public SslProtocols Protocol { get; init; }
+    public TlsCipherSuite? NegotiatedCipherSuite { get; init; }
+    public string? HostName { get; init; }
+    public SslApplicationProtocol NegotiatedApplicationProtocol { get; init; }
+}
